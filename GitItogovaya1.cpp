12b8c07f120d67f1e248e8e459357ac7f9be6f01
t <acella06@ya.ru>
2025-10-22 11:26:07 +0300

    On 3 task 1

---
+++ GitItogovaya1.cpp
@@ -0,0 +1,18 @@
+﻿// GitItogovaya1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+//#include <cmath>
+using namespace std;
+
+const double M_PI = 3.141592653589793;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int r;
+    cout << "Введите радиус"<<endl;
+    cin >> r;
+    cout << "Длина окружности: " << 2*r*M_PI <<endl;
+}
+
