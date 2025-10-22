1cbd6bc4c515fcf9af8a97c769ba15d879f16690
t <acella06@ya.ru>
2025-10-22 12:09:06 +0300

    On 4 task 1

---
+++ GitItogovaya2.cpp
@@ -0,0 +1,17 @@
+// GitItogovaya1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a,b;
+    cout << "Введите первую сторону: ";
+    cin >> a;
+    cout << "Введите вторую сторону: ";
+    cin >> b;
+    cout << "Периметр прямоугольника: " << 2 * (a+b) << endl;
+}
+
